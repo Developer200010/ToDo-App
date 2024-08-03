3896fd8b6ee2c466c8822689c719f33b6973b398
chandan vishwakarma <chandanvishw11002@gmail.com>
2024-08-03 16:17:58 +0530

    css done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AddToDo from './components/add-to'
 import ToDos from './components/todos'
 import Navbar from './components/navbar'
+import './globals.css'
 const page = () => {
   return (
     <main>
